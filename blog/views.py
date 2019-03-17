28e90bfe04a15f838c2920e7bf2979fbbd255266
wiame kazdari <kazdari.wiam@gmail.com>
2019-03-17 16:05:03 +0000

    update posts table to contain objects

--- blog/views.py
+++ blog/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 
 def index(request):
 
-    posts =['post1', 'post2', 'post3']
+    posts =[
+        {'id':1,'title':'first article', 'body':'my first article'},
+        {'id':2,'title':'second article', 'body':'my second article'},
+        {'id':3,'title':'third article', 'body':'my third article'},
+    ]
     return render(request, 'blog/index.html',{'posts':posts})
