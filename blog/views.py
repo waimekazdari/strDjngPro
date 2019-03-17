1870cd383d119449d3ae984625f25d73031722c8
wiame kazdari <kazdari.wiam@gmail.com>
2019-03-17 22:02:40 +0000

    details ...

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
 
+posts =[
+    {'id':1,'title':'first article', 'body':'my first article'},
+    {'id':2,'title':'second article', 'body':'my second article'},
+    {'id':3,'title':'third article', 'body':'my third article'},
+]
 def index(request):
 
     #posts= []
-    posts =[
-        {'id':1,'title':'first article', 'body':'my first article'},
-        {'id':2,'title':'second article', 'body':'my second article'},
-        {'id':3,'title':'third article', 'body':'my third article'},
-    ]
     return render(request, 'blog/index.html',{'posts':posts})
 
 
 def details(request,id):
-    return render(request, 'blog/details.html')
+    return render(request, 'blog/details.html', {'post': posts[int(id) - 1]})
