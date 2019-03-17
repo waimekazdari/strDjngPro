e18cb935d8e907da606a89fa285a8c372b0336d0
wiame kazdari <kazdari.wiam@gmail.com>
2019-03-17 16:28:16 +0000

    show a message when no post available

--- blog/views.py
+++ blog/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 
 def index(request):
 
-    posts =[
+    posts= []
+    '''posts =[
         {'id':1,'title':'first article', 'body':'my first article'},
         {'id':2,'title':'second article', 'body':'my second article'},
         {'id':3,'title':'third article', 'body':'my third article'},
-    ]
+    ]'''
     return render(request, 'blog/index.html',{'posts':posts})
